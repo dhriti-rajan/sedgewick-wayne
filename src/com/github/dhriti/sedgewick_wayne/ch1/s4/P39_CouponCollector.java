b04fb2a4ac5c28dc8a515bbdd993c009fdaa0804
Dhriti Rajan <dhriti.rajan@gmail.com>
2019-07-12 21:41:35 -0700

    Finished next exercise in section 1.4.
    This exercise simulates the number of draws needed to obtain all values
    in a deck from 0 to n, and shows that it can be approximated by n * Hn,
    where Hn is the nth harmonic number.

--- src/com/github/dhriti/sedgewick_wayne/ch1/s4/P39_CouponCollector.java
+++ src/com/github/dhriti/sedgewick_wayne/ch1/s4/P39_CouponCollector.java
@@ -3,8 +3,22 @@ package com.github.dhriti.sedgewick_wayne.ch1.s4;
 public class P39_CouponCollector {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
+        double averageDrawings = 0;
 
-        
+        for (int i = 0; i < 1000; ++i) {
+            int[] coupons = new int[n];
+
+            while(notFinished(coupons)) {
+                int coupon = (int) (Math.random() * n);
+                ++coupons[coupon];
+                ++averageDrawings;
+            }
+        }
+
+        averageDrawings /= 1000;
+
+        System.out.println(averageDrawings);
+        System.out.println(nthHarmonicNumber(n) * n);
     }
 
     private static double nthHarmonicNumber(int n) {
@@ -15,5 +29,12 @@ public class P39_CouponCollector {
         }
 
         return num;
+    }
+
+    private static boolean notFinished(int[] coupons) {
+        for (int i : coupons) {
+            if (i == 0) return true;
+        }
+        return false;
     }
 }
